c4613ee8372950ddbe89be6d4ea9b70e680f4ce9
Guillermo Barriga Placencia <guillermo@bitbox.cl>
2019-09-19 18:22:43 -0300

    muestra landing con navegación

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,10 +8,18 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+document.addEventListener("turbolinks:load", function() {
+  for (const button of [...document.querySelectorAll('i.fas.nav-button')]) {
+    button.addEventListener('click', (e) => {
+      document.querySelector('.shown').classList.replace('shown', 'hidden');
+      document.getElementById(`${button.dataset.goto}-container`).classList.replace('hidden', 'shown');
+    });
+  }
+});
